0f7c2af4cb56645cbdc2a97834a52fc11262171f
xiuzhenzhang <xiuzhen.zhang@rmit.edu.au>
2024-04-15 17:23:29 +1000

    Update shared.js

--- shared.js
+++ shared.js
@@ -4,15 +4,15 @@ document.write('\
   <div class="inner">\
   <div id="div1">\
   <h1 style="margin-bottom:0;"> Xiuzhen (Jenny) Zhang </h1>\
-  Associate Professor, PhD <br>\
-  School of Science (Computer Science and IT) <br>\
-  <a href=Òhttp://www.rmit.edu.au/Ò>RMIT University</a><br>\
+  Professor of Data Science, PhD <br>\
+  School of Computing Technologies <br>\
+  <a href=Ã’http://www.rmit.edu.au/Ã’>RMIT University</a><br>\
   GPO Box 2476<br>\
   Melbourne 3001, Australia<br>\
   Email: xiuzhen.zhang AT rmit.edu.au<br>\
   </div>\
   <div id="div2">\
-  <img src="images/zhang.jpg" width="130px" />\
+  <img src="XiuzhenZhang_small.jpeg" width="130px" />\
   </div>\
   </div>\
   </div>\
@@ -21,12 +21,11 @@ document.write('\
   <div id="menu">\
   <div class="center">\
    <nav>\
-    <a href="/"> <b> Home </b></a> :::\
+    <a href="/"> <b> Biography </b></a> :::\
     <a href="/publications/"><b>Publications</b></a> :::\
     <a href="/supervision/"><b>Supervision</b></a> :::\
     <a href="/grants/"><b>Grants</b></a> :::\
     <a href="/downloads/"><b>Downloads</b></a> :::\
-    <a href="/aboutme/"><b>About Me</b></a>\
    </nav>\
   </div>\
   </div>\
