363ec0e98d63050a07bbd9d54c2c4bdc00838e11
xiuzhenzhang <xiuzhen.zhang@rmit.edu.au>
2024-04-15 17:10:56 +1000

    Add files via upload

---
+++ shared.js
@@ -0,0 +1,36 @@
+document.write('\
+<!-- side-by-side divs inside a container div -->\
+  <div class="wrapper">\
+  <div class="inner">\
+  <div id="div1">\
+  <h1 style="margin-bottom:0;"> Xiuzhen (Jenny) Zhang </h1>\
+  Associate Professor, PhD <br>\
+  School of Science (Computer Science and IT) <br>\
+  <a href=Òhttp://www.rmit.edu.au/Ò>RMIT University</a><br>\
+  GPO Box 2476<br>\
+  Melbourne 3001, Australia<br>\
+  Email: xiuzhen.zhang AT rmit.edu.au<br>\
+  </div>\
+  <div id="div2">\
+  <img src="images/zhang.jpg" width="130px" />\
+  </div>\
+  </div>\
+  </div>\
+\
+<!-- navigation division -->\
+  <div id="menu">\
+  <div class="center">\
+   <nav>\
+    <a href="/"> <b> Home </b></a> :::\
+    <a href="/publications/"><b>Publications</b></a> :::\
+    <a href="/supervision/"><b>Supervision</b></a> :::\
+    <a href="/grants/"><b>Grants</b></a> :::\
+    <a href="/downloads/"><b>Downloads</b></a> :::\
+    <a href="/aboutme/"><b>About Me</b></a>\
+   </nav>\
+  </div>\
+  </div>\
+\
+<!-- horizontal line -->\
+  <hr style="width:80%; text-align:centre;"/>\
+')
